da690cf5b7f4e2000bbe29cc4bbedc2c96dd3c03
Carl Anderson <carander@starbucks.com>
2018-12-05 11:54:48 -0800

    Solution for 4b.

--- day4/4b.pl
+++ day4/4b.pl
@@ -16,6 +16,11 @@ my $currentGuard;
 my $sleepingMinutes = 0;
 my $startedSleepMinute;
 
+my @minutesGuards;
+my $maxMinutes = 0;
+my $maxMinuteValue = -1;
+my $maxGuard;
+
 foreach my $line (@inputLines) {
   my @bits = split(/ /, $line);
   my $date = $bits[0];
@@ -26,6 +31,20 @@ foreach my $line (@inputLines) {
 
   if ($log eq "w") {
     print "Guard $currentGuard started sleeping at $startedSleepMinute and ended at " . $times[1] . "\n";
+    for (my $i = $startedSleepMinute; $i < $times[1]; $i++) {
+      if (defined $minutesGuards[$i][$currentGuard]) {
+        $minutesGuards[$i][$currentGuard]++;
+      } else {
+        $minutesGuards[$i][$currentGuard] = 1;
+      }
+      print "value is " . $minutesGuards[$i][$currentGuard] . " for $i and max is $maxMinutes\n";
+      if ($minutesGuards[$i][$currentGuard] > $maxMinutes) {
+        print "It's greater so setting max minutes\n";
+        $maxMinutes = $minutesGuards[$i][$currentGuard];
+        $maxGuard = $currentGuard;
+        $maxMinuteValue = $i;
+      }
+    }
     $sleepingMinutes += $times[1] - $startedSleepMinute;
   } elsif ($log eq "s") {
     $startedSleepMinute = $times[1];
@@ -37,4 +56,6 @@ foreach my $line (@inputLines) {
     $sleepingMinutes = 0;
   }  
 }
+
+print "Guard is $maxGuard and minute is $maxMinuteValue. Multiplied = " . ($maxGuard * $maxMinuteValue) . "\n";
 
